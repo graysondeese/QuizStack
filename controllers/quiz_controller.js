43e1be9faa35586bcd38cbc79774c1ca22d47dd3
Grayson Deese <gldeese@ncsu.edu>
2020-06-15 13:58:48 -0400

    Skeleton for routes.

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,22 @@
+// Requiring express
+var express = require("express");
+// importing the model quiz.js
+var quiz = require("../models/quiz.js");
+// Defining routes on router
+var router = express.Router();
+
+// route for homepage
+router.get("/", function(req, res) {
+    
+  });
+
+// route for quiz
+router.get("/quiz", function(req, res) {
+    
+  });
+
+// route for highscores
+router.get("/highscores", function(req, res) {
+    
+  });
+  
