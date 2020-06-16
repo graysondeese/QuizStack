fbdd4f0fd4bccf765a4426b65355d6f9dc049c39
cglennon924 <63322716+cglennon924@users.noreply.github.com>
2020-06-16 14:09:09 -0400

    fixed merge on controller

--- public/assets/js/startquiz.js
+++ public/assets/js/startquiz.js
@@ -1,24 +1,23 @@
-// $(function(){
-//Make sure classes line up
-//     $(".startquiz").on("submit", function(event) {
-//         // Make sure to preventDefault on a submit event.
-//         event.preventDefault();
+$(function(){
+// Make sure classes line up
+    $("#").on("submit", function(event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
     
-//         var newQuestion = {
-//           question: $("#startBtn").val().trim(),
+        var newQuestion = {
+          question: $("#startBtn").val().trim(),
     
-//         };
+        };
     
-//         // Send the POST request.
-//         $.ajax("/controllers/quiz_controller.js", {
-//           type: "POST",
-//           data: newQuestion
-//         }).then(
-//           function() {
-//             console.log("New Question!");
-//             // Reload the page to get the updated list
-//             location.reload();
-//           }
-//         );
-//       });
-// });
+        // Send the POST request.
+        $.ajax("/highscores", {
+          type: "GET",
+        }).then(
+          function() {
+            console.log("High Scores!");
+            // Reload the page to get the updated list
+            location.reload();
+          }
+        );
+      });
+});
